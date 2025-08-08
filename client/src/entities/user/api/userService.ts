126e102d3e80ccb09089c840220e658310de6e8c
Kamil Gadzhiev <kamilgadzhiev2001@icloud.com>
2025-08-08 14:22:37 +0300

    axiosInsNeed

--- client/src/entities/user/api/userService.ts
+++ client/src/entities/user/api/userService.ts
@@ -1,9 +1,9 @@
-
-
+import type { UserLogin } from "../types/schema";
+import { UserResponseSchema, type UserRegister, type UserResponse } from "../types/schema";
 
 
 export class UserService { 
     static async signUp (formData: UserRegister) {
-        
+
     }
 }
